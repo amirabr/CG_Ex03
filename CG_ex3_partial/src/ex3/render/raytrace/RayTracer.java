022764c2104d63275ef8e131cfa4eedb6eee47b6
Amir <amirabr@gmail.com>
2015-04-09 23:08:36 +0300

    fixed RayTracer

--- CG_ex3_partial/src/ex3/render/raytrace/RayTracer.java
+++ CG_ex3_partial/src/ex3/render/raytrace/RayTracer.java
@@ -39,6 +39,7 @@ public class RayTracer implements IRenderer {
 	public void init(SceneDescriptor sceneDesc, int width, int height, File path) {
 		
 		scene = new Scene();
+		scene.init(sceneDesc.getSceneAttributes());
 		
 		for (Element e : sceneDesc.getObjects()) {
 			scene.addObjectByName(e.getName(), e.getAttributes());
@@ -69,14 +70,14 @@ public class RayTracer implements IRenderer {
 			
 			
 			Ray ray = scene.castRay(i, line, canvasWidth, canvasHeight);
-			/*Vec color = scene.calcColor(ray, 0);
-			Color realColor = new Color((int)color.x*255, (int)color.y*255, (int)color.z*255);
+			Vec color = scene.calcColor(ray, 0);
+			Color realColor = new Color((int)(color.x*255), (int)(color.y*255), (int)(color.z*255));
 			canvas.setRGB(i, line, realColor.getRGB());
-			*/
-			
+			//System.out.println("readColor=("+realColor.getRed()+", "+realColor.getGreen()+", "+realColor.getBlue());
+			/*
 			Color blue = new Color(0, 0, 255);
 			canvas.setRGB(i, line, blue.getRGB());
-			
+			*/
 		}
 		
 	}
